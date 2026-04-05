a7296e04893383bdb7dd9eb82387dd9fdb41210b
Anbu-00001 <anbuchelvanganesan.cse2024@citchennai.net>
2026-04-05 17:19:14 +0530

    chore: execute Zero-Trust Purge for HF Spaces Phase 1 evaluation

--- test_nlp.py
+++
@@ -1,6 +0,0 @@
-from env.environment import CrisisManagementEnv
-from env.reward import calculate_nlp_bonus
-env = CrisisManagementEnv(task_id=1)
-obs = env.reset(seed=42)
-msg = 'Downtown evacuate fire!'
-print("Score:", calculate_nlp_bonus(msg, obs))
